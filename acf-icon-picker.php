68cf496e16e9a753c164b68d1efe404f9fae8c07
Lamé Moatshe <lame633@yahoo.co.uk>
2023-02-16 10:32:43 +0000

    Updating plugin name to distinguish it from original

--- acf-icon-picker.php
+++ acf-icon-picker.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Advanced Custom Fields: Icon Picker
+Plugin Name: Advanced Custom Fields: Icon Picker (LGM Fork)
 Plugin URI: https://github.com/houke/acf-icon-picker
 Description: Allows you to pick an icon from a predefined list
 Version: 1.9.1
@@ -12,30 +12,31 @@ GitHub Plugin URI: https://github.com/houke/acf-icon-picker
 GitHub Branch: master
 */
 
-if( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
-if( !class_exists('acf_plugin_icon_picker') ) :
+if (!class_exists('acf_plugin_icon_picker')) :
 
-class acf_plugin_icon_picker {
+	class acf_plugin_icon_picker
+	{
 
-	function __construct() {
+		function __construct()
+		{
 
-		$this->settings = array(
-			'version'	=> '1.9.1',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
-		);
+			$this->settings = array(
+				'version'	=> '1.9.1',
+				'url'		=> plugin_dir_url(__FILE__),
+				'path'		=> plugin_dir_path(__FILE__)
+			);
 
-		add_action('acf/include_field_types', 	array($this, 'include_field_types'));
+			add_action('acf/include_field_types', 	array($this, 'include_field_types'));
+		}
 
+		function include_field_types($version = false)
+		{
+			include_once('fields/acf-icon-picker-v5.php');
+		}
 	}
 
-	function include_field_types( $version = false ) {
-		include_once('fields/acf-icon-picker-v5.php');
-	}
-
-}
-
-new acf_plugin_icon_picker();
+	new acf_plugin_icon_picker();
 
 endif;
